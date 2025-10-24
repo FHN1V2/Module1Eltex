2d8dcfc00a78da1e320f9e22fcdc0e931210138a
FHN1V2 <parfenov200407@gmail.com>
2025-10-24 09:45:38 +0700

    DisplayMenu

---
+++ module2/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+struct contact
+{
+    char* FullName;
+    char* WorkPlace;
+    char* Email;
+    char* Links;
+};
+
+void displayMenu();
+/*
+TODO:
+void addContact();
+void searchContact();
+void displayContacts();
+void editContact();
+void deleteContact();
+
+*/
+
+int main()
+{ 
+    int point;
+    
+    while (point!=0){
+        displayMenu();
+        printf("Выберите действие: ");
+        scanf("%d", &point);
+        switch(point) {
+            case 1:
+ //               addContact();
+                break;
+            case 2:
+ //               displayContacts();
+                break;
+            case 3:
+  //              editContact();
+                break;
+            case 4:
+  //              deleteContact();
+                break;
+            case 5:
+  //              searchContact();
+                break;
+            case 0:
+                printf("Выход из программы.\n");
+                break;
+            default:
+                printf("Неверный выбор!\n");
+        }
+    };
+
+    return 0;
+}
+
+
+
+
+
+void displayMenu()
+{
+    printf("\n=== МЕНЮ ===\n");
+    printf("1. Добавить контакт\n");
+    printf("2. Показать все контакты\n");
+    printf("3. Редактировать контакт\n");
+    printf("4. Удалить контакт\n");
+    printf("5. Поиск контакта\n");
+    printf("0. Выход\n");
+}
