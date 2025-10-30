5c96085b020c126b92799ce5580fe8c559fd41a9
FHN1V2 <parfenov200407@gmail.com>
2025-10-30 09:26:17 +0700

    Основные функции калькулятора

--- module2/2(3)/main.c
+++ module2/2(3)/main.c
@@ -26,7 +26,7 @@ double sub(int n, ...) {
     va_start(factor, n);
     double result = va_arg(factor, double);
     va_end(factor); 
-    va_start(factor+8, n); 
+    va_start(factor+8, n); // +8 из-за double
     for(int i = 1; i < n; i++) {
     result -= va_arg(factor, double); 
     }
@@ -44,12 +44,31 @@ double mult(int n, ...) {
     return result;
 }
 
+double del(int n, ...) {
+    va_list factor; 
+    va_start(factor, n);
+    double result = va_arg(factor, double);
+    printf("%f \n", result);
+    va_end(factor); 
+    va_start(factor+8, n); // +8 из-за double
+    for(int i = 1; i < n; i++) {
+    double arg = va_arg(factor,double);
+        if (arg==0.0) // Проверка на наличие 0 при делении 
+        {
+            printf("Inpun ZERO");
+            break;
+        }
+    result /= arg;
+    }
+    va_end(factor); 
+    return result;
+}
+
 int main() {
 
     printf("%f \n", sum(4, 1.2, 2.0, 3.0, 4.0));
-    printf("%f \n", sum(5, 12.1, 21.7, 13.9, 4.0, 5.0));
     printf("%f \n", sub(3, 100.0, 32.0, 10.9));
     printf("%f \n", mult(3, 2.0, 3.0, 4.0));
+    printf( "%f \n", del(3, 10.0, 5.0, 2.0));
     return 0;
-
 }
